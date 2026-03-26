a02370d8f46dc500ea7a0e24c6cdd4aa077b148e
Ismat07 <khan.ismat711@gmail.com>
2026-03-26 16:56:50 +0530

    working on  nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import { Link } from "react-router-dom";
-import styles from './Nav.module.css'
+import styles from './Nav.module.css';
+import logo from '../../assets/images/logo.jpeg';
+/*import { useState } from "react";*/
 
 const Nav = () => {
   return (
+    <Nav className={styles.navbar}>
+      <div>
+        <img src={logo} alt="Logo"   width="200px" height="100px" />
+      </div>
     <div className={styles['topnav']} id="myTopnav">
       <Link to="/home">Home</Link>
       <Link to="/contact">Contact</Link>
@@ -17,10 +22,9 @@ const Nav = () => {
           <Link to="/instit-gallery">Institute</Link>
         </div>
       </div>
-      <Link to="/login">Login</Link>
-
-
-    </div>
+  <Link to="/login">Login</Link>
+</div>
+</Nav>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,7 @@ import styles from './Home.module.css';
 
 const Home = () => {
     return (
-        <div>
             <div className={styles['main-container']}>
-                <Header />
                 <Nav />
                 <div className={styles['banner-container']}>
                     <img src={banner} alt="Banner" className={styles['banner-img']} />
@@ -18,22 +16,18 @@ const Home = () => {
                         <h2>Enquiry Form</h2>
                         <form id="contactForm">
                             <div className={styles['form-group']}>
-                                <label htmlFor="name">Name</label>
                                 <input type="text" id="name" name="name" placeholder="Your Name" required />
                                 <span className={styles['error-message']} id="nameError"></span>
                             </div>
                             <div className={styles['form-group']}>
-                                <label htmlFor="email">Email</label>
                                 <input type="email" id="email" name="email" placeholder="Your Email" required />
                                 <span className={styles['error-message']} id="emailError"></span>
                             </div>
                             <div className={styles['form-group']}>
-                                <label htmlFor="phone">Phone</label>
                                 <input type="tel" id="phone" name="phone" placeholder="Your Phone Number" required />
                                 <span className={styles['error-message']} id="phoneError"></span>
                             </div>
                             <div className={styles['form-group']}>
-                                <label htmlFor="message">Message</label>
                                 <textarea id="message" name="message" placeholder="Your Message" rows="5" required ></textarea>
                                 <span className={styles['error-message']} id="messageError"></span>
                             </div>
@@ -45,7 +39,7 @@ const Home = () => {
                 </div>
                 <Footer />
             </div>
-        </div>
+        
     );
 };
 
