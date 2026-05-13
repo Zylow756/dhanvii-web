62ca4b4911c14188032a8127ca8678f81ea3d19d
Zylow756 <zylow0744@gmail.com>
2026-05-13 12:07:03 +0530

    changes in app.listen()

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 // SERVER
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
